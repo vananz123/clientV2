6b4c4de492523a0a135ef05001fd165c0b553764
nguyendinhlinh65@gmail.com <nguyendinhlinh65@gmail.com>
2024-06-29 21:21:17 +0700

    fix-search

--- src/pages/ProductListShow/ProductFilter.tsx
+++ src/pages/ProductListShow/ProductFilter.tsx
@@ -6,23 +6,23 @@ import React from 'react';
 import { ArrayToString } from '@/utils/utils';
 interface Props {
     filter: Filter;
-    setQueryString: (name: string, alue: string) => void;
-    removeQueryString:(name: string) => void;
+    setQueryString: (name: string, value: string) => void;
+    removeQueryString: (name: string) => void;
 }
-const ProductFilter: React.FC<Props> = ({ filter, setQueryString,removeQueryString }) => {
+const ProductFilter: React.FC<Props> = ({ filter, setQueryString, removeQueryString }) => {
     const handleChangeSort = (value: Sort) => {
         setQueryString('sortOder', value);
     };
-    const onChangeOpPrice = (value: number[]) => {
+    const onChangeOpPrice = (value: any[]) => {
         if (typeof value !== 'undefined') {
-            removeQueryString('page')
+            removeQueryString('page');
             const valueQuery = ArrayToString(value);
             setQueryString('optionPrice', valueQuery);
         }
     };
     const onChangeOpSize = (value: string[]) => {
         if (typeof value !== 'undefined') {
-            removeQueryString('page')
+            removeQueryString('page');
             const valueQuery = ArrayToString(value);
             setQueryString('optionMaterial', valueQuery);
         }
@@ -30,60 +30,103 @@ const ProductFilter: React.FC<Props> = ({ filter, setQueryString,removeQueryStri
     const onChangePromotion = () => {
         if (filter.isPromotion) {
             const isPromotion: string = filter.isPromotion ? '0' : '1';
-            removeQueryString('page')
+            removeQueryString('page');
             setQueryString('isPromotion', isPromotion);
         } else {
-            removeQueryString('page')
+            removeQueryString('page');
             setQueryString('isPromotion', '1');
         }
     };
+    interface ItemProps {
+        label: string;
+        value: string;
+    }
+    const options: ItemProps[] = [];
+    if (filter.optionPrice && filter.optionMaterial) {
+        const combinedArray = filter.optionPrice.map((number) => number.toString()).concat(filter.optionMaterial);
+        options.push({
+            label: `${combinedArray}`,
+            value: `${combinedArray}`,
+        });
+    } else if (filter.optionPrice) {
+        options.push({
+            label: `${filter.optionPrice}`,
+            value: `${filter.optionPrice}`,
+        });
+    } else {
+        if (filter.optionMaterial) {
+            options.push({
+                label: `${filter.optionMaterial}`,
+                value: `${filter.optionMaterial}`,
+            });
+        }
+    }
+    console.log(options);
     return (
-        <Flex justify="space-between" gap={16} style={{ marginBottom: '10px' }}>
-            <Space wrap>
+        <div>
+            {filter.optionPrice || filter.optionMaterial ? (
+                <Space>
+                    <Select mode="multiple" className="w-full mb-1" value={options} options={options} onChange={onChangeOpPrice} />
+                </Space>
+            ) : (
+                ''
+            )}
+            <Flex justify="space-between" gap={16} style={{ marginBottom: '20px' }}>
+                <Space wrap style={{ width: '100%' }}>
+                    <Select
+                        mode="multiple"
+                        style={{ width: 250 }}
+                        options={OPTIONS_PRICE}
+                        value={filter.optionPrice}
+                        placeholder="Price"
+                        maxTagCount="responsive"
+                        onChange={onChangeOpPrice}
+                    />
+                    {/* <Select
+                        mode="multiple"
+                        style={{ width: 150 }}
+                        options={OPTIONS_PRICE}
+                        autoFocus={false}
+                        showSearch={false}
+                        value={filter.optionPrice}
+                        onChange={onChangeOpPrice}
+                        placeholder="Price"
+                        maxTagCount="responsive"
+                        maxTagPlaceholder={(omittedValues) => (
+                            <Tooltip title={omittedValues.map(({ label }) => label).join(', ')}>
+                                <span>Hover Me</span>
+                            </Tooltip>
+                        )}
+                    /> */}
+                    <Select
+                        mode="multiple"
+                        style={{ width: 150 }}
+                        autoFocus={false}
+                        showSearch={false}
+                        options={OPTIONS_MATERIAL}
+                        value={filter.optionMaterial}
+                        onChange={onChangeOpSize}
+                        placeholder="Brand"
+                        maxTagCount="responsive"
+                        maxTagPlaceholder={(omittedValues) => (
+                            <Tooltip title={omittedValues.map(({ label }) => label).join(', ')}>
+                                <span>Hover Me</span>
+                            </Tooltip>
+                        )}
+                    />
+                    <Checkbox checked={filter.isPromotion} style={{ width: 150 }} onChange={onChangePromotion}>
+                        Có khuyến mãi
+                    </Checkbox>
+                </Space>
                 <Select
-                    mode="multiple"
-                    style={{ width: 150 }}
-                    options={OPTIONS_PRICE}
+                    value={filter.sortOder}
                     autoFocus={false}
-                    showSearch={false}
-                    value={filter.optionPrice}
-                    onChange={onChangeOpPrice}
-                    placeholder="Price"
-                    maxTagCount="responsive"
-                    maxTagPlaceholder={(omittedValues) => (
-                        <Tooltip title={omittedValues.map(({ label }) => label).join(', ')}>
-                            <span>Hover Me</span>
-                        </Tooltip>
-                    )}
+                    style={{ width: 125 }}
+                    onChange={handleChangeSort}
+                    options={OPTIONS_SORT}
                 />
-                <Select
-                    mode="multiple"
-                    style={{ width: 150 }}
-                    autoFocus={false}
-                    showSearch={false}
-                    options={OPTIONS_MATERIAL}
-                    value={filter.optionMaterial}
-                    onChange={onChangeOpSize}
-                    placeholder="Brand"
-                    maxTagCount="responsive"
-                    maxTagPlaceholder={(omittedValues) => (
-                        <Tooltip title={omittedValues.map(({ label }) => label).join(', ')}>
-                            <span>Hover Me</span>
-                        </Tooltip>
-                    )}
-                />
-                <Checkbox checked={filter.isPromotion} style={{ width: 120 }} onChange={onChangePromotion}>
-                    Có khuyến mãi
-                </Checkbox>
-            </Space>
-            <Select
-                value={filter.sortOder}
-                autoFocus={false}
-                style={{ width: 125 }}
-                onChange={handleChangeSort}
-                options={OPTIONS_SORT}
-            />
-        </Flex>
+            </Flex>
+        </div>
     );
 };
 
